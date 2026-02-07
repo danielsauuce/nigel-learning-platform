226fdb344bba9be660572c03b2811f5fecb488b3
Daniel <danieladeyd77@gmail.com>
2026-02-07 08:57:28 +0000

    tailwind, linting and workflow implemented

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,46 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const prettier = require('eslint-plugin-prettier');
 
 module.exports = defineConfig([
   expoConfig,
+
   {
-    ignores: ['dist/*'],
+    ignores: ['dist/*', 'node_modules/*', '.expo/*', 'expo-env.d.ts'],
+  },
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier,
+    },
+    rules: {
+      'react-native/no-inline-styles': 'off', // NativeWind uses className
+      'react-native/split-platform-components': 'off',
+
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+
+      'import/order': [
+        'warn',
+        {
+          groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling']],
+          'newlines-between': 'always',
+        },
+      ],
+
+      'prettier/prettier': 'warn',
+    },
   },
 ]);
