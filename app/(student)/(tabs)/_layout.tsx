33ade9ecf5027bf95a0e53f32ee729216df20457
“david <davzy007@gmail.com>
2026-02-09 23:09:06 +0000

    added dashboard screen

---
+++ app/(student)/(tabs)/_layout.tsx
@@ -0,0 +1,9 @@
+import { Tabs } from "expo-router";
+
+export default function StudentTabScreen() {
+  return (
+    <Tabs screenOptions={{ headerShown: false, animation: 'shift' }}>
+      <Tabs.Screen name="Dashboard" />
+    </Tabs>
+  )
+}
