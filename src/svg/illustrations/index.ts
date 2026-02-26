c3cde069bc3ac2aa74883dd2ffdb5d621b3a2dfc
danielsauuce <danieladeyd77@gmail.com>
2026-02-26 15:20:46 +0000

    Role selection screens implemented

--- src/svg/illustrations/index.ts
+++ src/svg/illustrations/index.ts
@@ -2,3 +2,5 @@ export { Mascot } from './Mascot';
 export { IslandIcon } from './IslandIcon';
 export { TrophyIcon } from './TrophyIcon';
 export { PiggyIcon } from './PiggyIcon';
+export { StudentIllustration } from './StudentIllustration';
+export { TeacherIllustration } from './TeacherIllustration';
