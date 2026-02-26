853549457d66db9565014a86d55dcaeb5eebc6cc
danielsauuce <danieladeyd77@gmail.com>
2026-02-26 15:26:11 +0000

    auth: personalization and teachlogin screen created

---
+++ app/(auth)/personalization.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const PersonalizationRoute = () => {
+  return (
+    <View>
+      <Text>personalization</Text>
+    </View>
+  );
+};
+
+export default PersonalizationRoute;
---
+++ app/(auth)/teacher-login.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const TeacherLoginRoute = () => {
+  return (
+    <View>
+      <Text>TeacherLoginRoute</Text>
+    </View>
+  );
+};
+
+export default TeacherLoginRoute;
