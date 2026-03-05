e2883433510d08a8812ecfb9698b21e5964d5600
Daniel <danieladeyd77@gmail.com>
2026-03-05 21:26:20 +0000

    life event implemented

---
+++ src/components/student/sim-results/index.ts
@@ -0,0 +1,4 @@
+export { SimResultsScreen } from './SimResultsScreen';
+export { ResultsScoreCard } from './ResultsScoreCard';
+export { ResultsBreakdown } from './ResultsBreakdown';
+export { ResultsFeedbackList } from './ResultsFeedbackList';
