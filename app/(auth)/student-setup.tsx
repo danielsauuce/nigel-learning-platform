3636047fdc038fe95f87b1c35be955c28b72b703
Daniel <danieladeyd77@gmail.com>
2026-03-05 17:19:40 +0000

    student auth modified and routing fixed

---
+++ app/(auth)/student-setup.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StudentSetupScreen } from '@/components/auth/student-setup/StudentSetupScreen';
+
+export default function StudentSetupRoute() {
+  return <StudentSetupScreen />;
+}
---
+++ src/components/auth/student-setup/index.ts
@@ -0,0 +1,6 @@
+export { StudentSetupScreen } from './StudentSetupScreen';
+export { StudentSetupHeader } from './StudentSetupHeader';
+export { StudentSetupMascot } from './StudentSetupMascot';
+export { StudentSetupForm } from './StudentSetupForm';
+export { AgePicker } from './AgePicker';
+export { StudentIdPreview } from './StudentIdPreview';
