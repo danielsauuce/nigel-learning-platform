36d15849f79f22ba746a639eae2a5127a03b03c3
Daniel <danieladeyd77@gmail.com>
2026-03-05 18:13:43 +0000

    budgetbuilder: screen, card, route, slider and feedback implemented

---
+++ src/components/student/budget-builder/index.ts
@@ -0,0 +1,6 @@
+export { BudgetBuilderScreen } from './BudgetBuilderScreen';
+export { BudgetBuilderHeader } from './BudgetBuilderHeader';
+export { BudgetBalanceCard } from './BudgetBalanceCard';
+export { BudgetBreakdownBar } from './BudgetBreakdownBar';
+export { BudgetSlider } from './BudgetSlider';
+export { BudgetFeedback } from './BudgetFeedback';
