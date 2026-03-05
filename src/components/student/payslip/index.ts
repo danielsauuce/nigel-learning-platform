88cfbf7fb11d437b4d3d7be5ba1d794eb8dd7475
Daniel <danieladeyd77@gmail.com>
2026-03-05 17:35:41 +0000

    payslip folder implemented

---
+++ src/components/student/payslip/index.ts
@@ -0,0 +1,5 @@
+export { PayslipScreen } from './PayslipScreen';
+export { PayslipHeader } from './PayslipHeader';
+export { PayslipSummaryCard } from './PayslipSummaryCard';
+export { PayslipBreakdownChart } from './PayslipBreakdownChart';
+export { PayslipTip } from './PayslipTip';
