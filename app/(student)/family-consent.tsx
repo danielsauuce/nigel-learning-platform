c25c54b97c541f32766566f8ade60cc19a77bc8b
Daniel Omogbolahan Adeyemi <6adeyd77@m-b112-79148.uni.solent.ac.uk>
2026-03-06 17:32:15 +0000

    parent summary and sharing link implemented

---
+++ app/(student)/family-consent.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ConsentScreen } from '@/components/student/family-share/ConsentScreen';
+
+export default function FamilyConsentRoute() {
+  return <ConsentScreen />;
+}
